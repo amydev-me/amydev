ea0ba851b32c527a3b359ccf2aa6a4bd0c08e120
Amy <ampyaephyonaing@gmail.com>
2019-08-23 08:42:43 +0630

    Fixed UI

--- js/theme.js
+++ js/theme.js
@@ -1,4 +1,3 @@
-
 (function($) {
 	'use strict';
 
@@ -21,7 +20,20 @@
 		}
 	}
 	navbarFixed();
+	// var skillsDiv = jQuery('#skills');
 
+	// jQuery(window).on('scroll', function(){
+	// 	var winT = jQuery(window).scrollTop(),
+	// 	winH = jQuery(window).height(),
+	// 	skillsT = skillsDiv.offset().top;
+	// if(winT + winH  > skillsT){
+	// 	jQuery('.skillbar').each(function(){
+	// 	jQuery(this).find('.skillbar-bar').animate({
+	// 		width:jQuery(this).attr('data-percent')
+	// 	},1000);
+	// 	});
+	// }
+	// });
 	/*----------------------------------------------------*/
 	/*  MailChimp Slider
     /*----------------------------------------------------*/
@@ -308,18 +320,3 @@
 		});
 	}
 })(jQuery);
-
-var skillsDiv = jQuery('#skills');
-
-	jQuery(window).on('scroll', function(){
-		var winT = jQuery(window).scrollTop(),
-		winH = jQuery(window).height(),
-		skillsT = skillsDiv.offset().top;
-	if(winT + winH  > skillsT){
-		jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},1000);
-		});
-	}
-	});
