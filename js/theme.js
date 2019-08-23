295306d5ec22b3665665a6702def22329f3d3318
Amy <ampyaephyonaing@gmail.com>
2019-08-23 09:01:53 +0630

    theme.js

--- js/theme.js
+++ js/theme.js
@@ -20,20 +20,7 @@
 		}
 	}
 	navbarFixed();
-	var skillsDiv = jQuery('#skills');
-
-	jQuery(window).on('scroll', function(){
-		var winT = jQuery(window).scrollTop(),
-		winH = jQuery(window).height(),
-		skillsT = skillsDiv.offset().top;
-	if(winT + winH  > skillsT){
-		jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},1000);
-		});
-	}
-	});
+	
 	/*----------------------------------------------------*/
 	/*  MailChimp Slider
     /*----------------------------------------------------*/
@@ -320,3 +307,17 @@
 		});
 	}
 })(jQuery);
+var skillsDiv = jQuery('#skills');
+
+	jQuery(window).on('scroll', function(){
+		var winT = jQuery(window).scrollTop(),
+		winH = jQuery(window).height(),
+		skillsT = skillsDiv.offset().top;
+	if(winT + winH  > skillsT){
+		jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},1000);
+		});
+	}
+	});
