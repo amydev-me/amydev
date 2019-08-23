e6666676f0b38c7cd9165eafb52d19754229e5a9
Amy <ampyaephyonaing@gmail.com>
2019-08-23 14:34:29 +0630

    Send mail

--- js/theme.js
+++ js/theme.js
@@ -309,15 +309,15 @@
 })(jQuery);
 var skillsDiv = jQuery('#skills');
 
-	jQuery(window).on('scroll', function(){
-		var winT = jQuery(window).scrollTop(),
-		winH = jQuery(window).height(),
-		skillsT = skillsDiv.offset().top;
-	if(winT + winH  > skillsT){
-		jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},1000);
-		});
-	}
-	});
+// jQuery(window).on('scroll', function(){
+// 	var winT = jQuery(window).scrollTop(),
+//   	winH = jQuery(window).height(),
+//   	skillsT = skillsDiv.offset().top;
+//   if(winT + winH  > skillsT){
+  	jQuery('.skillbar').each(function(){
+      jQuery(this).find('.skillbar-bar').animate({
+        width:jQuery(this).attr('data-percent')
+      },1000);
+    });
+//   }
+// });
